da4a9c97a3927fd51ee830e577c1e53356bf2727
Sagiru20 <sagiru203@gmail.com>
2024-03-06 16:03:44 +0200

    Write username and createdAt component directly in comment component

--- src/Components/Reusable/CreatedAt.tsx
+++
@@ -1,7 +0,0 @@
-import { Typography } from "@mui/material";
-
-const CreatedAt = ({ createdAt }) => {
-    return <Typography sx={{ color: "hsl(211, 10%, 45%)" }}>{createdAt}</Typography>;
-};
-
-export default CreatedAt;

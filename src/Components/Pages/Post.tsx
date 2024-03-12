e8fb21edee8d3828ad6579222a660c36d3ee40ae
Sagiru20 <sagiru203@gmail.com>
2024-03-12 20:45:34 +0200

    Make edit button appear in post only if the owner is in the page

--- src/Components/Pages/Post.tsx
+++ src/Components/Pages/Post.tsx
@@ -1,11 +1,5 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import EditButton from "../Reusable/Buttons/TextButtons/EditButton";
-import PostFormModal from "../Posts/PostFormModal";
-
-import useAxiosPrivate from "../../hooks/useAxiosPrivate";
-import usePostService from "../../services/posts-services";
-
 import {
     Box,
     Card,
@@ -16,20 +10,30 @@ import {
     Grid,
     Typography,
     Stack,
+    Button,
 } from "@mui/material";
-import PageNotFound from "./PageNotFound";
-import CommentSection from "../CommentSection";
+import { Edit } from "@mui/icons-material";
 import { IPost } from "../../Models";
+import useAuth from "../../hooks/useAuth";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import usePostService from "../../services/posts-services";
+import PageNotFound from "./PageNotFound";
+import PostFormModal from "../Posts/PostFormModal";
+import CommentSection from "../CommentSection";
+import useUserService from "../../services/user-services";
 
 function Post() {
+    const { auth } = useAuth();
     const { id } = useParams<{ id: string }>();
     const backendPrivateClient = useAxiosPrivate();
     const { getPost } = usePostService(backendPrivateClient);
+    const { getUser } = useUserService(backendPrivateClient);
 
     const [post, setPost] = useState<IPost | null>(null);
     const [loading, setLoading] = useState(true);
     const [showPostFormModal, setShowPostFormModal] = useState(false);
     const [isPostChanged, setIsPostChanged] = useState(false);
+    const [ownerName, setOwnerName] = useState<string | undefined>();
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -37,14 +41,41 @@ function Post() {
                 const post = await getPost(id!);
                 post && setPost(post);
             } catch (error) {
-                console.error("Error fetching posts:", error);
+                console.error("Error fetching posts: ", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        const getOwnerDetails = async () => {
+            try {
+                const ownerDetails = await getUser(post?.ownerId);
+                ownerDetails && setOwnerName(`${ownerDetails.firstName} ${ownerDetails.lastName}`);
+            } catch (error) {
+                console.error("Error fetching post owner details: ", error);
             } finally {
                 setLoading(false);
             }
         };
 
         fetchPost();
+        getOwnerDetails();
     }, [id, isPostChanged]);
+
+    // useEffect(() => {
+    //     const getOwnerDetails = async () => {
+    //         try {
+    //             const ownerDetails = await getUser(post?.ownerId);
+    //             ownerDetails && setOwnerName(`${ownerDetails.firstName} ${ownerDetails.lastName}`);
+    //         } catch (error) {
+    //             console.error("Error fetching post owner details: ", error);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
+
+    //     getOwnerDetails();
+    // }, []);
 
     if (loading) {
         return (
@@ -90,7 +121,13 @@ function Post() {
 
                     <Grid item xs={7} sx={{ display: "flex", flexDirection: "column" }}>
                         <Card sx={{ borderRadius: 3, height: "100%" }}>
-                            <CardContent>
+                            <CardContent
+                                sx={{
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    paddingBottom: "16px !important",
+                                }}
+                            >
                                 <Typography
                                     gutterBottom
                                     variant="h5"
@@ -107,10 +144,6 @@ function Post() {
                                     {post.title}
                                 </Typography>
 
-                                <EditButton
-                                    functionality={() => setShowPostFormModal(true)}
-                                    editingComm={false}
-                                />
                                 <PostFormModal
                                     isOpen={showPostFormModal}
                                     postId={id}
@@ -118,12 +151,10 @@ function Post() {
                                 />
 
                                 <Typography variant="body1" component="div" sx={{ mb: 1 }}>
-                                    {/* TODO: Call backend to get owner name */}
-                                    Created by {post.ownerId}, last modified:{" "}
-                                    {post.updatedAt.toLocaleString()}
+                                    Created by {ownerName}, last modified: {post.updatedAt.toLocaleString()}
                                 </Typography>
 
-                                <Stack direction="column" spacing={2}>
+                                <Stack direction="column" spacing={2} sx={{ mb: 3 }}>
                                     {Object.entries(post.dogInfo).map(
                                         ([key, value]) =>
                                             key !== "_id" && (
@@ -145,6 +176,7 @@ function Post() {
                                                 </Box>
                                             )
                                     )}
+
                                     <Box
                                         display="flex"
                                         flexDirection="row"
@@ -158,6 +190,29 @@ function Post() {
                                         <Typography variant="body1">{post.city ?? "-"}</Typography>
                                     </Box>
                                 </Stack>
+
+                                {auth.userInfo?._id == post.ownerId && (
+                                    <Button
+                                        variant="contained"
+                                        size="medium"
+                                        startIcon={<Edit />}
+                                        onClick={() => {
+                                            setShowPostFormModal(true);
+                                        }}
+                                        sx={{
+                                            display: "flex",
+                                            alignSelf: "flex-end",
+                                            backgroundColor: "hsl(238, 40%, 52%)",
+                                            textTransform: "capitalize",
+                                            "&:hover": {
+                                                opacity: 0.8,
+                                                backgroundColor: "hsl(238, 40%, 52%)",
+                                            },
+                                        }}
+                                    >
+                                        Edit Post
+                                    </Button>
+                                )}
                             </CardContent>
                         </Card>
                     </Grid>

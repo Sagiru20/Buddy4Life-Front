88f0ca1a8c47b73dc27e413f801d5cd394429b9b
Sagiru20 <sagiru203@gmail.com>
2024-03-14 21:21:45 +0200

    Add back breeds page

--- src/Components/Pages/Breeds.tsx
+++ src/Components/Pages/Breeds.tsx
@@ -1,5 +1,56 @@
+import { useState, useEffect } from "react";
+import { Box, Container, Grid } from "@mui/material";
+import { IBreed } from "../../Models";
+import { getBreeds } from "../../DogBreedApi";
+import BreedCard from "../BreedCard";
+
 function Breeds() {
-    return <div className="App">Breeds</div>;
+    const [breeds, setBreeds] = useState<IBreed[]>([]);
+
+    useEffect(() => {
+        const fetchBreeds = async () => {
+            try {
+                const breeds = await getBreeds();
+                breeds && setBreeds(breeds);
+            } catch (error) {
+                console.error("Error fetching breeds:", error);
+            }
+        };
+
+        fetchBreeds();
+    }, []);
+
+    return (
+        <Box
+            sx={{
+                mt: 5,
+                mb: 5,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}
+        >
+            <Container>
+                <Grid container spacing={12} display="flex">
+                    {breeds.map((breed, index) => (
+                        <Grid
+                            item
+                            xs={12}
+                            sm={12}
+                            md={6}
+                            lg={4}
+                            xl={4}
+                            key={index}
+                            display="flex"
+                            justifyContent="center"
+                        >
+                            <BreedCard breed={breed} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </Box>
+    );
 }
 
 export default Breeds;

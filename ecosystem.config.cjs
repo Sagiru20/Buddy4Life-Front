5d1e2dbc8e7d8face2bc5c5123fb253fdf8b3d56
Sagiru20 <sagiru203@gmail.com>
2024-03-14 20:52:19 +0200

    Change port to 4000

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "front",
-      script: "vite preview --port 443 --host --config vite.config.ts",
+      script: "vite preview --port 4000 --host --config vite.config.ts",
       env_production: {
         NODE_ENV: "production",
       },
